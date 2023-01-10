319c57b47936a26af30c77b828082a27332f67a5
yonyoo <yonyoo@student.42seoul.kr>
2023-01-10 14:01:17 +0900

    Update Shell01 ex08

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,1 +1,1 @@
-echo 'ibase=5;obase=23;' $(echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!' "1234") + $(echo $FT_NBR2 | tr 'mrdoc' "01234") | bc #| tr '0123456789ABC' 'gtaio\ luSnemf'
+echo 'ibase=5;obase=23;' $(echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!' "1234") + $(echo $FT_NBR2 | tr 'mrdoc' "01234") | bc | tr '0123456789ABC' 'gtaio\ luSnemf'
