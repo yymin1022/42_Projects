70e939f0223efed77a0013c57d66c21d014de2b0
yonyoo <yonyoo@student.42seoul.kr>
2023-01-12 10:07:58 +0900

    Update C00 ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,49 +6,36 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:33:42 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/11 17:06:36 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/12 10:07:33 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_get_comb(char c[3])
-{
-	char	tmp[3];
-
-	tmp = c;
-	while (tmp[0] <= 7)
-	{
-		while (tmp[1] <= 8)
-		{
-			if (tmp[0] != tmp[1])
-			{
-				while (tmp[2] <= 9)
-				{
-					if (tmp[1] != tmp[2])
-					{
-						write (1, tmp, 1);
-						write (1, tmp + 1, 1);
-						write (1, tmp + 2, 1);
-					}
-					tmp[2]++;
-				}
-			}
-			tmp[1]++;
-		}
-		tmp = c;
-		tmp[0]++;
-	}
-}
 
 void	ft_print_comb(void)
 {
 	char	c[3];
 
 	c[0] = '0';
-	c[1] = '1';
-	c[2] = '2';
-	ft_get_comb (c);
+	while (c[0] <= '7')
+	{
+		c[1] = c[0] + 1;
+		while (c[1] <= '8')
+		{
+			c[2] = c[1] + 1;
+			while (c[2] <= '9')
+			{
+				write(1, c, 1);
+				write(1, c + 1, 1);
+				write(1, c + 2, 1);
+				if (c[0] != '7' || c[1] != '8' || c[2] != '9')
+				{
+					write(1, ", ", 2);
+				}
+				c[2]++;
+			}
+			c[1]++;
+		}
+		c[0]++;
+	}
 }
-
-int main(){ft_print_comb();}
