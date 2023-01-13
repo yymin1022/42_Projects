518147e5225b58052bcce439474656da447f9002
yonyoo <yonyoo@student.42seoul.kr>
2023-01-13 20:47:06 +0900

    Update C00 ex08

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,30 +6,59 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 18:40:16 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/12 19:27:18 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/13 20:46:33 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_combn(int n)
+void	print_num(int *num, int n)
 {
-	char num[9];
-	int idx;
+	char	tmp;
+	int		idx;
 
 	idx = 1;
-	num[0] = '0';
 	while (idx < n)
 	{
-		num[idx] = num[idx - 1] + 1;
+		if (*(num + idx) <= *(num + idx - 1))
+			return ;
+		idx++;
 	}
-	
-	while (num[0] <= 10 - n)
+	idx = 0;
+	while (idx < n)
 	{
-		
+		tmp = *(num + idx) + '0';
+		write(1, &tmp, 1);
+		idx++;
 	}
+	if (*num < 10 - n)
+		write(1, ", ", 2);
 }
 
-int main(){
-	ft_print_combn(2);
+void	ft_print_combn(int n)
+{
+	int	idx;
+	int	num[10];
+
+	idx = 0;
+	while (idx < n)
+	{
+		num[idx] = idx;
+		idx++;
+	}
+	while (num[0] <= 10 - n)
+	{
+		print_num(num, n);
+		idx = n - 1;
+		num[idx]++;
+		while (idx > 0)
+		{
+			if (num[idx] > 9)
+			{
+				num[idx] = 0;
+				num[idx - 1]++;
+			}
+			idx--;
+		}
+	}
 }
