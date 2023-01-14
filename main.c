efa7c0c7e1ab26b860e0c4f4fde3a453bb5b15fd
yonyoo <yonyoo@student.42seoul.kr>
2023-01-14 13:59:10 +0900

    Update Rush00

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 10:10:17 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/14 13:30:17 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/14 13:54:04 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@ extern void	rush(int x, int y);
 int	main(void)
 {
 	rush (5, 3);
+	rush (-1, 3);
+	rush (5, -1);
+	rush (-1, -1);
 	return (0);
 }
--- rush00.c
+++ rush00.c
@@ -6,17 +6,62 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:32:24 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/14 13:36:33 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/14 13:56:59 by yonyoo           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */ 
+/* ************************************************************************** */
 
 extern void	ft_putchar(char c);
+
+void	print_error(char c)
+{
+	ft_putchar('E');
+	ft_putchar('r');
+	ft_putchar('r');
+	ft_putchar(':');
+	ft_putchar(' ');
+	ft_putchar(c);
+	ft_putchar(' ');
+	ft_putchar('i');
+	ft_putchar('s');
+	ft_putchar(' ');
+	ft_putchar('N');
+	ft_putchar('e');
+	ft_putchar('g');
+	ft_putchar('a');
+	ft_putchar('t');
+	ft_putchar('i');
+	ft_putchar('v');
+	ft_putchar('e');
+	ft_putchar('\n');
+}
+
+int	check_error(int x, int y)
+{
+	int	flag;
+
+	flag = 0;
+	if (x < 0)
+	{
+		flag = 1;
+		print_error('x');
+	}
+	if (y < 0)
+	{
+		flag = 1;
+		print_error('y');
+	}
+	if (flag)
+		return (-1);
+	return (0);
+}
 
 void	rush(int x, int y)
 {
 	int	i;
 	int	j;
 
+	if (check_error(x, y) == -1)
+		return ;
 	i = 0;
 	while (i < y)
 	{
