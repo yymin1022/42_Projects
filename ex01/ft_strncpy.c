08965beb5bd6d5478371c8f75e6c15613c2ab07c
yonyoo <yonyoo@student.42seoul.kr>
2023-01-15 13:55:08 +0900

    Update C02 ex01

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:09:10 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/14 23:01:27 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/15 13:54:49 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		*(dest + idx) = *(src + idx);
 		idx++;
 	}
-	while (idx < sizeof(dest) && idx < n)
+	while (idx < n)
 	{
 		*(dest + idx) = '\0';
 		idx++;
