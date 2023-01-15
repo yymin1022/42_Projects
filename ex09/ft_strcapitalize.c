7eb07d1ba8ae490a1ee6f48795d3224c96031373
yonyoo <yonyoo@student.42seoul.kr>
2023-01-15 17:48:36 +0900

    Update C02 ex09

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,30 +6,43 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:22:57 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/15 13:55:48 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/15 17:46:23 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int	is_alpha_num(char str)
+{
+	if (str >= 'A' && str <= 'Z')
+		return (1);
+	else if (str >= 'a' && str <= 'z')
+		return (2);
+	else if (str >= '0' && str <= '9')
+		return (3);
+	else
+		return (0);
+}
+
 char	*ft_strcapitalize(char *str)
 {
 	int	idx;
 
-	if (*str >= 'a' && *str <= 'z')
+	idx = 0;
+	while (*(str + idx))
+	{
+		if (is_alpha_num(*(str + idx)) == 1)
+			*(str + idx) += 32;
+		idx++;
+	}
+	if (is_alpha_num(*str) == 2)
 		*str -= 32;
 	idx = 1;
 	while (*(str + idx))
 	{
-		if (*(str + idx) >= 'a' && *(str + idx) <= 'z')
+		if (is_alpha_num(*(str + idx)) == 2)
 		{
-			if (*(str + idx - 1) < '0')
-				*(str + idx) -= 32;
-			else if (*(str + idx - 1) > '9' && *(str + idx - 1) < 'A')
-				*(str + idx) -= 32;
-			else if (*(str + idx - 1) > 'Z' && *(str + idx - 1) < 'a')
-				*(str + idx) -= 32;
-			else if (*(str + idx - 1) > 'z')
+			if (is_alpha_num(*(str + idx - 1)) == 0)
 				*(str + idx) -= 32;
 		}
 		idx++;
