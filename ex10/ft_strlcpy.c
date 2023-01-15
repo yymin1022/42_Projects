d72d6c59ee1c760e8da4ace1dff9a5ae51b2d599
yonyoo <yonyoo@student.42seoul.kr>
2023-01-15 10:43:41 +0900

    Update C02 ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:46:45 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/15 10:18:54 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/15 10:43:31 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	idx;
 
 	idx = 0;
-	while ((*(src + idx)) && (*(dest + idx)) && idx < size)
+	while ((*(src + idx)) && idx < sizeof(dest) && idx < size)
 	{
 		*(dest + idx) = *(src + idx);
 		idx++;
 	}
 	idx--;
 	*(dest + idx) = '\0';
-	return (idx);
+	return (size + 1);
 }
