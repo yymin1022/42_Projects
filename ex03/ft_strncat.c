446d1f8a337dd9a6862340130bbd72358ceb24b0
yonyoo <yonyoo@student.42seoul.kr>
2023-01-15 21:30:46 +0900

    Update C03 ex03

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 20:07:45 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/01/15 21:30:36 by yonyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	idx;
+	int				size;
+
+	idx = 0;
+	size = 0;
+	while (*(dest + size))
+	{
+		size++;
+	}
+	while (*(src + idx) && idx < nb)
+	{
+		*(dest + size + idx) = *(src + idx);
+		idx++;
+	}
+	*(dest + size + idx) = '\0';
+	return (dest);
+}
