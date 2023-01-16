8ec12444e524a31b395151347b71c0d141dcb97d
yonyoo <yonyoo@student.42seoul.kr>
2023-01-16 23:00:31 +0900

    Update C04 ex03

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 20:10:31 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/01/16 23:00:24 by yonyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	check_heading(char *str, int *idx, int *sign)
+{
+	while (*(str + *idx))
+	{
+		if (*(str + *idx) == '+' || *(str + *idx) == ' ')
+			(*idx)++;
+		else if (*(str + *idx) == '-')
+		{
+			*sign = -(*sign);
+			(*idx)++;
+		}
+		else
+			break ;
+	}
+}
+
+int	ft_atoi(char *str)
+{
+	int	ans;
+	int	idx;
+	int	sign;
+
+	ans = 0;
+	idx = 0;
+	sign = 1;
+	check_heading(str, &idx, &sign);
+	while (*(str + idx))
+	{
+		if (*(str + idx) >= '0' && *(str + idx) <= '9')
+		{
+			ans *= 10;
+			ans += *(str + idx) - '0';
+		}
+		else
+			break;
+		idx++;
+	}
+	return (ans * sign);
+}
