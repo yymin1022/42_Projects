4a4f0eb69202d33bc8e66d4fb71a44a7dc98403e
yonyoo <yonyoo@student.42seoul.kr>
2023-01-16 13:54:37 +0900

    Update C03 ex01

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 19:44:46 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/15 19:48:30 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/16 13:54:11 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	idx;
 
 	idx = 0;
-	while (*(s1 + idx) && *(s2 + idx) && idx < n)
+	while (*(s1 + idx) && *(s2 + idx) && *(s1 + idx) == *(s2 + idx) && idx < n)
 	{
-		if (*(s1 + idx) > *(s2 + idx))
-			return (1);
-		else if (*(s1 + idx) < *(s2 + idx))
-			return (-1);
 		idx++;
 	}
-	return (0);
+	return (*(s1 + idx) - *(s2 + idx));
 }
