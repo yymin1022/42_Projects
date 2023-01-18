81eecbb06c0f6af8b6728ac3e250f20422aee0bc
yonyoo <yonyoo@student.42seoul.kr>
2023-01-18 13:21:23 +0900

    Update C06 ex00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 13:13:39 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/01/18 13:21:16 by yonyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char *c)
+{
+	while (*c)
+		write(1, c++, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*name;
+
+	name = argv[argc - argc];
+	ft_putchar(name);
+}
