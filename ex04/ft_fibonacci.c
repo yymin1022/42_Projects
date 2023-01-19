5a9a9b9fa0f2ea7d864acf4c318f98caded8bcd7
yonyoo <yonyoo@student.42seoul.kr>
2023-01-19 10:44:17 +0900

    Update C05 ex04

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:13:41 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/19 10:25:06 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/19 10:43:54 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	if (index <= 2)
+	if (index < 2)
 		return (index);
 	else
 		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
