c338dcb2785fca21b71ae525b510d1a8ffbc38b8
yonyoo <yonyoo@student.42seoul.kr>
2023-01-19 10:35:58 +0900

    Update C05 ex01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:00:14 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/19 10:02:00 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/19 10:35:48 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb == 1)
+	if (nb <= 1)
 		return (1);
 	else
 		return (nb * ft_recursive_factorial(nb - 1));
