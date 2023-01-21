201634c662eeb42454c648289f95673d6163bd6d
yonyoo <yonyoo@student.42seoul.kr>
2023-01-21 19:27:26 +0900

    Update C05 ex02

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:02:42 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/21 10:07:23 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/21 19:24:14 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,6 @@ int	ft_iterative_power(int nb, int power)
 
 	if (power < 0)
 		return (0);
-	if (nb == 0)
-	{
-		if (power == 0)
-			return (1);
-		else
-			return (0);
-	}
 	result = 1;
 	while (power--)
 		result *= nb;
