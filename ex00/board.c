202c70fd6b83910cc7f89ef3210c0bf558f11074
yonyoo <yonyoo@student.42seoul.kr>
2023-01-21 14:00:22 +0900

    Update Rush01

--- ex00/board.c
+++ ex00/board.c
@@ -6,25 +6,29 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 12:54:14 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/21 13:27:47 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/21 14:00:16 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
 
 void	init_board_side(int **board, char *arg)
 {
 	int	i;
 
 	i = 0;
-	while (arg[i])
+	while (i < 16)
 	{
 		if (i < 4)
-			board[0][i] = arg[i];
+			board[0][i + 1] = arg[i * 2] - '0';
 		else if (i < 8)
-			board[5][i - 4] = arg[i];
-		else if (i < 16)
-			board[i - 8][0] = arg[i];
+			board[5][i - 3] = arg[i * 2] - '0';
+		else if (i < 12)
+			board[i - 7][0] = arg[i * 2] - '0';
 		else
-			board[i - 12][5] = arg[i];
+			board[i - 11][5] = arg[i * 2] - '0';
+		i++;
 	}
 }
 
@@ -58,9 +62,10 @@ void	print_board(int **board)
 		j = 0;
 		while (j < 6)
 		{
-			ft_putstr(board[i][j] - '0');
+			ft_putchar(board[i][j] + '0');
 			j++;
 		}
+		ft_putchar('\n');
 		i++;
 	}
 }
--- ex00/main.c
+++ ex00/main.c
@@ -6,17 +6,22 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 11:26:33 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/21 12:53:59 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/21 13:48:08 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 int	check_arg(char *arg);
+void	ft_putstr(char *str);
 void	init_board(int **board, char *arg);
+void	print_board(int **board);
 void	print_err(int err);
 
 int main(int argc, char **argv)
 {
-	int	board[6][6];
+	int	**board;
+	int	tmp;
 
 	if (argc != 2)
 	{
@@ -28,6 +33,14 @@ int main(int argc, char **argv)
 		print_err(0);
 		return (0);
 	}
+	tmp = 0;
+	board = (int **)malloc(sizeof(int *) * 6);
+	while (tmp < 6)
+	{
+		board[tmp] = (int *)malloc(sizeof(int) * 6);
+		tmp++;
+	}
 	init_board(board, argv[1]);
+	print_board(board);
 	return (0);
 }
