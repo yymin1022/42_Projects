75a03ecf7b76d1fb7ff2a001d9d36bfe6c9e3aab
yonyoo <yonyoo@student.42seoul.kr>
2023-01-21 10:12:36 +0900

    Update C05 ex03

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:05:23 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/19 10:40:20 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/21 10:12:29 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@ int	ft_recursive_power(int nb, int power)
 {
 	if (power < 0)
 		return (0);
-	if (power <= 1)
+	if (nb == 0)
+	{
+		if (power == 0)
+			return (1);
+		else
+			return (0);
+	}
+	if (power == 0)
 		return (1);
-	else
-		return (nb * ft_recursive_power(nb, power - 1));
+	return (nb * ft_recursive_power(nb, power - 1));
 }
