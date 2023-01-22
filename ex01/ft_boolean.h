191214cf6b433442115ee44443660fd3ac70995c
yonyoo <yonyoo@student.42seoul.kr>
2023-01-22 18:00:38 +0900

    Update C08 ex01

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,18 +6,20 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:57:33 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/22 17:29:43 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/22 17:59:52 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-# define EVEN(N) N % 2
-# define EVEN_MSG "I have an even number of arguments"
-# define ODD_MSG "I have an odd number of arguments"
-# define SUCCESS 1
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define FALSE 0
+# define TRUE 1
+# define SUCCESS 0
+# define EVEN(N) !((N) % 2)
 # include <unistd.h>
 
-typedef enum	t_bool { FALSE = 0, TRUE = 1 } t_bool;
+typedef int	t_bool;
 
 #endif
