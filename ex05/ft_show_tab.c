ff681801246d5c9d5012221f243af8543b3b8e8b
yonyoo <yonyoo@student.42seoul.kr>
2023-01-23 13:37:39 +0900

    Update C08 ex05

---
+++ ex05/ft_show_tab.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 11:11:51 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/01/23 13:34:15 by yonyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+
+void	ft_putnbr(int n)
+{
+	char	tmp;
+
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+	{
+		tmp = n + '0';
+		write(1, &tmp, 1);
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	idx;
+
+	idx = 0;
+	while (*(str + idx))
+	{
+		write(1, str + idx, 1);
+		idx++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	idx;
+
+	idx = 0;
+	while (*(par + idx)->str)
+	{
+		ft_putstr((par + idx)->str);
+		ft_putnbr((par + idx)->size);
+		ft_putstr((par + idx)->copy);
+		idx++;
+	}
+}
