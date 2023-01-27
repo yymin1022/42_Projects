1a046ff6d9653a3115cc145c9f06cd3256c04572
yonyoo <yonyoo@student.42seoul.kr>
2023-01-27 19:04:52 +0900

    Update C02 ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,11 +6,11 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:28:54 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/01/23 10:39:04 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/01/27 19:03:57 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-Int	ft_str_is_alpha(char *str)
+int	ft_str_is_alpha(char *str)
 {
 	int	idx;
 
