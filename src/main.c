ef0715820bcdb447e5ee9936d3701c96faea507f
yonyoo <yonyoo@student.42seoul.kr>
2023-02-01 17:55:18 +0900

    Update BSQ : Fixed Norm

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sangylee <sangylee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 10:43:59 by sangylee          #+#    #+#             */
-/*   Updated: 2023/02/01 15:43:48 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/02/01 17:54:18 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,8 @@ int	get_result(char *file_name)
 	map_char = (char **)malloc(sizeof(char *) * cord.x);
 	map_cnt = (int **)malloc(sizeof(int *) * cord.x);
 	init_map(map_char, map_cnt, cord.x, cord.y);
-	if (init_map_char(file_name, map_char, cord.y, size_data) != cord.x - 2)
-		return (-1);
-	if (!init_map_cnt(map_char, map_cnt, map_info + size_data - 3, cord))
+	if (init_map_char(file_name, map_char, cord.y, size_data) != cord.x - 2
+		|| !init_map_cnt(map_char, map_cnt, map_info + size_data - 3, cord))
 	{
 		free_memory(map_char, map_cnt, map_info, cord);
 		return (-1);
