f851d1309489543fe463cd0f8e5c7109f4d541f7
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 12:02:31 +0900

    Update libft : ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,11 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 11:32:03 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 11:41:54 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 12:02:23 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	isdigit(int c)
+int	ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
