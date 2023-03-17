f801b4c7cb68706ee0537bddb0c794e1e4f4612a
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 16:04:04 +0900

    Update libft : ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:58:07 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 14:38:46 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 16:02:49 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -17,12 +19,17 @@ int	ft_atoi(const char *str)
 	int		res;
 
 	i = 0;
+	while (*(str + i) && (((char)*(str + i) >= 9 && (char)*(str + i) <= 13)
+			|| ((char)*(str + i) == 32)))
+		i++;
 	neg_flag = 1;
-	if (*(str) == '-')
+	if (*(str + i) == '-')
 	{
 		i++;
 		neg_flag = -1;
 	}
+	else if (*(str + i) == '+')
+		i++;
 	res = 0;
 	while (*(str + i))
 	{
