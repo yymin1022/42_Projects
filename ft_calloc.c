6f8c9eeaa8280ef36b73bc434fe6f8a2b4aa3b25
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 18:48:30 +0900

    Update libft : ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,20 +6,19 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 14:23:38 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 14:28:24 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 16:06:27 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
-void	*calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*res;
 
 	res = malloc(count * size);
 	if (!res)
 		return (0);
-	ft_bzero(tmp, count * size);
-	return (tmp);
+	ft_bzero(res, count * size);
+	return (res);
 }
