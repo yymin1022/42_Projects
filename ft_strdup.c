d6c69c3c17ea3ab11f361cf11b7f1c196ec0d9cc
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 18:49:25 +0900

    Update libft : ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,11 +6,12 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 22:14:29 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 14:22:27 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 18:49:15 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strdup(const char *s1)
 {
@@ -22,6 +23,8 @@ char	*ft_strdup(const char *s1)
 	while (*(s1 + src_size))
 		src_size++;
 	res = (char *)malloc(src_size + 1);
+	if (!res)
+		return (0);
 	idx = 0;
 	while (idx < src_size)
 	{
