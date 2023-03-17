c6fdc26d74898a0cf5aa14902cc6c6ee8be5e9c1
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 18:14:54 +0900

    Update libft : ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,9 +6,11 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 11:24:48 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 12:02:04 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 14:51:23 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isalpha(int c)
 {
