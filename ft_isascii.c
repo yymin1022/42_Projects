8cde3ff7552e8b5e1790ccf6065aed5de7abe96a
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 18:15:05 +0900

    Update libft : ft_isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,9 +6,11 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 11:38:32 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 12:03:05 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 14:49:05 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isascii(int c)
 {
