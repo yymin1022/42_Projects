a0f82581075044c6e9c40486739040d32ffcc002
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 18:47:57 +0900

    Update libft : ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,27 +6,31 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:46:49 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 14:38:14 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 18:47:37 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
+	if (*needle == '\0')
+		return ((char *)haystack);
 	i = 0;
-	while (*(haystack + i) && i < len)
+	while (*(char *)(haystack + i) && i < len)
 	{
 		j = 0;
-		while (*(needle + j))
+		while (*(char *)(needle + j) && i + j < len)
 		{
-			if (*(haystack + i + j) != *(needle + j))
+			if (*(char *)(haystack + i + j) != *(needle + j))
 				break ;
 			j++;
 		}
 		if (*(needle + j) == 0)
-			return (haystack + i);
+			return ((char *)(haystack + i));
 		i++;
 	}
 	return (0);
