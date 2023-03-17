062e69de4058d0a233a620679790e7c1f26c9753
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 14:23:32 +0900

    Update libft : ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,15 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:58:07 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 14:20:46 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 14:23:25 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	atoi(const char *str)
 {
 	size_t	i;
-	int	neg_flag;
-	int	res;
+	int		neg_flag;
+	int		res;
 
 	i = 0;
 	neg_flag = 1;
