77b08ceaf42f6413322e3a684aa0de1bf1fdcbec
yonyoo <yonyoo@student.42seoul.kr>
2023-03-17 16:57:20 +0900

    Update libft : ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:09:02 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 16:51:13 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/17 16:55:48 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t			i;
 
-	if (dst == src)
+	if (!dst && !src)
 		return (dst);
 	i = 0;
 	while (i < n)
