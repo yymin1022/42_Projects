37571cd537f033f5a8d5bb2399a8acee2a151867
yonyoo <yonyoo@student.42seoul.kr>
2023-03-20 21:31:05 +0900

    Update libft : ft_strjoin

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 14:28:52 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/20 20:22:37 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/20 21:25:43 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
