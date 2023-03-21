3442b856b2f065307eb6c9898f432ada124c020f
yonyoo <yonyoo@student.42seoul.kr>
2023-03-21 20:03:43 +0900

    Update libft : ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:06:51 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 14:55:56 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/21 20:01:37 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	char	*res;
 	size_t	i;
 
 	i = 0;
-	res = (char *)s;
 	while (i < n)
 	{
-		*(res + i) = 0;
+		*((char *)s + i) = 0;
 		i++;
 	}
 }
