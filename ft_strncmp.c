0fdabdf2f6bff49d5663979640d2bfac7b071573
yonyoo <yonyoo@student.42seoul.kr>
2023-03-21 22:23:54 +0900

    Update libft : ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 19:44:46 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 18:23:48 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/21 22:23:39 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
-	size_t			i;
+	size_t	i;
 
 	if (!n)
 		return (0);
 	i = 0;
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
-	while (*(str1 + i) && *(str2 + i)
-		&& *(str1 + i) == *(str2 + i) && i < n - 1)
+	while (*(s1 + i) && *(s2 + i)
+		&& *((unsigned char *)s1 + i)
+		== *((unsigned char *)s2 + i) && i < n - 1)
 		i++;
-	return (*(str1 + i) - *(str2 + i));
+	return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
 }
