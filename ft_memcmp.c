5ac45a2d0694b1f6096c9f5402b06904018fe8b3
yonyoo <yonyoo@student.42seoul.kr>
2023-03-23 11:08:38 +0900

    Update libft : ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:41:45 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 18:39:42 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/23 11:08:29 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,15 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*res1;
-	unsigned char	*res2;
-	size_t			i;
+	size_t	i;
 
 	if (!n)
 		return (0);
 	i = 0;
-	res1 = (unsigned char *)s1;
-	res2 = (unsigned char *)s2;
 	while (i < n)
 	{
-		if (*(res1 + i) != *(res2 + i))
-			return (*(res1 + i) - *(res2 + i));
+		if (*((unsigned char *)s1 + i) != *((unsigned char *)s2 + i))
+			return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
 		i++;
 	}
 	return (0);
