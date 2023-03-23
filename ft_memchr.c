af374bb2af6356556525c46fe0b130d2a8a915bd
yonyoo <yonyoo@student.42seoul.kr>
2023-03-23 11:03:48 +0900

    Update libft : ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:36:58 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/17 18:40:35 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/23 10:37:13 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
+	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		if (*((unsigned char *)(s + i)) == (unsigned char)c)
-			return ((unsigned char *)(s + i));
+		if (*((unsigned char *)s + i) == (unsigned char)c)
+			return ((unsigned char *)s + i);
 		i++;
 	}
 	return (0);
