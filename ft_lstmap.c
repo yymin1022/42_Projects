4cacf020f11f59f20043c5354beb58c5a03158b8
yonyoo <yonyoo@student.42seoul.kr>
2023-03-23 18:34:48 +0900

    Update libft : ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 18:26:34 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/03/23 18:30:54 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/03/23 18:34:40 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
-	t_list *res_next;
+	t_list	*res_next;
 
 	res = 0;
 	while (lst->next)
