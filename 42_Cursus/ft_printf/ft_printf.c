3e53fffe96c5e9ea60249d76864b558eb2c8e43d
yymin1022 <yymin1022@gmail.com>
2023-07-01 01:38:08 +0900

    Update ft_printf

--- 42_Cursus/ft_printf/ft_printf.c
+++ 42_Cursus/ft_printf/ft_printf.c
@@ -6,11 +6,51 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 18:45:19 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/06/30 21:49:12 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2023/07/01 01:37:20 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+static void	print_format(va_list ap, const char format, int *cnt)
+{
+	if (format == 'c')
+		print_char(va_arg(ap, char), cnt);
+	else if (format == 's')
+		print_str(va_arg(ap, char *), cnt);
+	else if (format == 'p')
+		print_ptr(va_arg(ap, void *), cnt);
+	else if (format == 'd' || format == 'i')
+		print_dec(va_arg(ap, int), cnt);
+	else if (format == 'u')
+		print_u_dec(va_arg(ap, unsigned int), cnt);
+	else if (format == 'x')
+		print_hex_lower(va_arg(ap, int), cnt);
+	else if (format == 'X')
+		print_hex_upper(va_arg(ap, int), cnt);
+	else if (format == '%')
+		print_char('%', cnt);
+}
+
 int	ft_printf(const char *str, ...)
 {
-	
+	int		cnt;
+	int		idx;
+	va_list	ap;
+
+	cnt = 0;
+	idx = 0;
+	va_start(ap, str);
+	while (*(str + idx))
+	{
+		if (*(str + idx) == '%')
+			print_format(ap, *(str + ++idx), &cnt);
+		else
+			print_char(*(str + idx), &cnt);
+		idx++;
+	}
+}
+
+int main(){
+	ft_printf("c %c s %s p %p d %d i %i u %u x %x X %X % %%", "qwer");
 }
