60e21fd52ab99b1c3d664cddd6f4130dbe97c799
yymin1022 <yymin1022@gmail.com>
2023-06-30 21:51:20 +0900

    Update ft_printf

--- 42_Cursus/ft_printf/ft_printf.h
+++ 42_Cursus/ft_printf/ft_printf.h
@@ -1,16 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/30 18:45:19 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/06/30 21:49:12 by yonyoo           ###   ########seoul.kr  */
+/*   Created: 2023/06/30 21:49:42 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/06/30 21:51:08 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *str, ...)
-{
-	
-}
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
