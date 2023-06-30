eb595b7de421a88fef79b8f59d04bbc02ab8b88c
yymin1022 <yymin1022@gmail.com>
2023-07-01 02:01:10 +0900

    Update ft_printf

---
+++ 42_Cursus/ft_printf/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/17 14:23:38 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/03/17 16:06:27 by yonyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*res;
+
+	res = malloc(count * size);
+	if (!res)
+		return (0);
+	ft_bzero(res, count * size);
+	return (res);
+}
