37396dd3f69af93e44467eebfec654fa754b4cd5
yymin1022 <yymin1022@gmail.com>
2023-07-01 01:36:19 +0900

    Update ft_printf

--- 42_Cursus/ft_printf/ft_printf.h
+++ 42_Cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 21:49:42 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/06/30 21:52:01 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2023/07/01 01:36:07 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define FT_PRINTF_H
 # include <stdarg.h>
 
-int	ft_printf(const char *str, ...);
+int			ft_printf(const char *str, ...);
+static void	print_format(va_list ap, const char format, int *cnt);
+static void	print_char(char c, int *cnt);
+static void	print_str(char *str, int *cnt);
+static void	print_ptr(void *ptr, int *cnt);
+static void	print_dec(int num, int *cnt);
+static void	print_u_dec(unsigned int num, int *cnt);
+static void	print_hex_lower(int num, int *cnt);
+static void	print_hex_upper(int num, int *cnt);
 
 #endif
