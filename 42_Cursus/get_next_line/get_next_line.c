b789c591de545035d3ef04b2f784113c75f132ca
yonyoo <yonyoo@student.42seoul.kr>
2023-08-05 22:21:07 +0900

    Update get_next_line

--- 42_Cursus/get_next_line/get_next_line.c
+++ 42_Cursus/get_next_line/get_next_line.c
@@ -6,7 +6,19 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 06:00:23 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/07/28 06:00:24 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2023/08/05 22:20:59 by yonyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE];
+	char		*line;
+	static char	*backup;
+
+	read(fd, buf, BUFFER_SIZE);
+
+	return (line);
+}
---
+++ 42_Cursus/get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/05 20:13:23 by yonyoo            #+#    #+#             */
+/*   Updated: 2023/08/05 22:20:57 by yonyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
