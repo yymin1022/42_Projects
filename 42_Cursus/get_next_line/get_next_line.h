7091eed62ca05247db159564d6e54a6fee53d38b
yymin1022 <yymin1022@gmail.com>
2023-08-10 02:42:34 +0900

    Update get_next_line

--- 42_Cursus/get_next_line/get_next_line.h
+++ 42_Cursus/get_next_line/get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 20:13:23 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/08/05 22:20:57 by yonyoo           ###   ########.fr       */
+/*   Updated: 2023/08/06 17:13:21 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 42
 # include <stdlib.h>
 # include <unistd.h>
 
