c9000c7f698654cec76234568539fa0dd9181794
Yymin1022 <yymin1022@gmail.com>
2023-10-08 02:21:21 +0900

    Update minitalk

--- 42_Cursus/minitalk/server.c
+++ 42_Cursus/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 01:49:43 by yonyoo            #+#    #+#             */
-/*   Updated: 2023/10/08 02:01:41 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2023/10/08 02:21:16 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,35 @@
 
 static void	ft_sighandler(int signal)
 {
+	static char	data;
+	static int	bit;
 
+	if (signal == SIGUSR1)
+		data = 0;
+	else if (signal == SIGUSR2)
+		data = 1;
+	if (bit < 7)
+		data <<= 1;
+	bit++;
+	if (bit == 8)
+	{
+		// print data
+		bit = 0;
+		data = 0;
+	}
 }
 
 static void	ft_print_pid(pid_t pid)
 {
-	// write pid
+	// print pid
 }
 
 int	main(int argc, char** argv)
 {
-	pid_t	pid;
-
-	if (argc != 1)
-	{
-		return (0);
-	}
-	pid = getpid();
-	ft_print_pid(pid);
+	ft_print_pid(getpid());
 	signal(SIGUSR1, ft_sighandler);
 	signal(SIGUSR2, ft_sighandler);
-	while(1)
+	while (1)
 		pause();
 	return (0);
 }
